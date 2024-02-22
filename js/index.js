616a0d246cceb5a8bad73a103988cba5680fde38
taiful-18 <taifulh902@gmail.com>
2024-02-22 19:27:17 +0600

    totaly done game

--- js/index.js
+++ js/index.js
@@ -1,32 +1,87 @@
-function hendelKbdButtonUp(event){
-    const plyerPresdKey = event.key;
-    console.log(plyerPresdKey);
+function hendelKbdButtonUp(event) {
+    // plyer pressed 
+    const plyerPressedKey = event.key;
 
 
     // get the expect to pressed
     const corentAlphaBateEliment = document.getElementById('current-alphabate');
     const corentAlphaBate = corentAlphaBateEliment.innerText;
     const expectedAlphaBate = corentAlphaBate.toLowerCase();
-    console.log(expectedAlphaBate , plyerPresdKey);
+
+    // check matched or not 
+    if (plyerPressedKey === expectedAlphaBate) {
+        const removebg = document.getElementById(plyerPressedKey);
+        removebg.classList.remove('bg-[#FFA500]');
+        removebg.classList.remove('text-black');
+
+        // update score 
+        // const yourScore = document.getElementById('your-score');
+        // const scoreText = yourScore.innerText;
+        // const coruntScore = parseInt(scoreText);
+
+        // element value by use function 
+        const scoreValue = getTextElementValueById('your-score');
+        const newScore = scoreValue + 1;
+        const scoreTextSet = setTextElementValueById('your-score', newScore);
+
+
+        continueGame();
+        // console.log('you get score');
+    }
+    else {
+        // update life
+        // const yourLife = document.getElementById('your-life');
+        // const lifeText = yourLife.innerText ;
+        // const coruntLife = parseInt(lifeText);
+        const lifeValue = getTextElementValueById('your-life');
+        const newLife = lifeValue - 1;
+        const lifeTextSet = setTextElementValueById('your-life', newLife);
+
+        // change the screen or end the game and show the score
+        if (newLife === 0) {
+            // hide playground section
+            hideElementById('play-ground');
+
+            // show the result-page section
+            showElementById('result-page');
+            const resultScoreGet = document.getElementById('your-score').innerText;
+            setTextElementValueById('result-score', resultScoreGet)
+        }
+
+
+        // const newLife = coruntLife - 1 ;
+        // yourLife.innerText = newLife ;
+    }
 }
 document.addEventListener('keyup', hendelKbdButtonUp);
 
 
 
-function continueGame(){
+function continueGame() {
     const alphabet = getRendomAlfabate();
-    console.log(alphabet);
     const currentAlphaBate = document.getElementById('current-alphabate');
     const confarmAlphaBate = currentAlphaBate.innerText = alphabet;
     setKyeBackgroundColor(confarmAlphaBate);
 }
 
-function play(){
+function play() {
     // step one : hide the home section
-    const homeButton = document.getElementById('home-section');
-    homeButton.classList.add('hidden');
-    const playGround = document.getElementById('play-ground');
-    playGround.classList.remove('hidden')
+    hideElementById('home-section');
+    showElementById('play-ground')
     continueGame()
 }
 
+function playAgain() {
+    showElementById('play-ground');
+    hideElementById('result-page');
+    setTextElementValueById('your-score', 0);
+    setTextElementValueById('your-life', 5);
+    const coreantAlphaBate = document.getElementById('current-alphabate').innerText;
+    const lastBgRemove = document.getElementById(coreantAlphaBate);
+    lastBgRemove.classList.remove('bg-[#FFA500]');
+    lastBgRemove.classList.remove('text-black');
+    // coreantAlphaBate.classList.remove('bg-[#FFA500]');
+    continueGame();
+
+
+}
--- js/sesundindex.js
+++ js/sesundindex.js
@@ -1,7 +1,15 @@
+function hideElementById(elementid){
+    const element = document.getElementById(elementid);
+    element.classList.add('hidden');
+}
+function showElementById(elementid){
+    const element = document.getElementById(elementid);
+    element.classList.remove('hidden');
+}
 function getRendomAlfabate(){
     // create a rendom alfabate
     const alphabetString = 'abcdefghijklmnopqrstubwxyz';
-    const alphaBates = alphabetString.split('')
+    const alphaBates = alphabetString.split('');
     const RandomNumber = Math.random() * 25;
     const index = Math.round(RandomNumber)
     const alphabet = alphaBates[index];
@@ -11,4 +19,17 @@ function getRendomAlfabate(){
 function setKyeBackgroundColor(elimentid){
     const kyeBackground = document.getElementById(elimentid);
     kyeBackground.classList.add('bg-[#FFA500]');
+    kyeBackground.classList.add('text-black');
+}
+
+function getTextElementValueById(elementid){
+    const element = document.getElementById(elementid);
+    const elementValueText = element.innerText;
+    const elementValue = parseFloat(elementValueText);
+    return elementValue
+
+}
+function setTextElementValueById(elimentid , value){
+    const element = document.getElementById(elimentid);
+    element.innerText = value;
 }
